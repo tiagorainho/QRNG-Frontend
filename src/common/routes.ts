2d508b9085c977a2a7da8cd4621aef82cbfff3f2
tiago <tiago.rainho@ua.pt>
2021-12-02 01:37:20 +0000

    feat: show current page on navbar

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -3,8 +3,9 @@ import Qrng from '../qrng/ui/Qrng.svelte';
 import Documentation from '../documentation/ui/Documentation.svelte'
 
 export const routes = {
-    // Exact path
+    // root
     '/': Documentation,
+
     '/qrng': Qrng
 }
 
--- src/common/ui/Navbar.svelte
+++ src/common/ui/Navbar.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+    import {location} from 'svelte-spa-router'
+    const selected_color = 'text-red-400';
+</script>
+
+
 <div class="flex flex-col font-mono">
     <nav class="flex justify-between bg-white h-12 px-4 md:px-32">
 
@@ -14,16 +20,15 @@
 
         <!-- center -->
         <ul class="flex items-center gap-10 flex-grow justify-end">
-            <a href="/#" class="hover:no-underline">Documentation</a>
-            <a href="/#/api" class="hover:no-underline">API</a>
-            <a href="/#/qrng" class="hover:no-underline">QRNG</a>
-            <a href="/#/about" class="hover:no-underline">About</a>
+            <a href="/#" class="hover:no-underline {$location == '/' ? selected_color : ''}">Documentation</a>
+            <a href="/#/api" class="hover:no-underline {$location == '/api' ? selected_color : ''}">API</a>
+            <a href="/#/qrng" class="hover:no-underline {$location == '/qrng' ? selected_color : ''}">QRNG</a>
+            <a href="/#/about" class="hover:no-underline {$location == '/about' ? selected_color : ''}">About</a>
         </ul>
 
         <!-- right  -->
         {#if false}
         <ul class="flex items-center">
-
             <li class="pr-6">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +45,6 @@
                         d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
                     <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
                 </svg>
-
             </li>
             <li class="h-10 w-10">
                 <img
@@ -48,7 +52,6 @@
                     src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                     alt="profile woman" />
             </li>
-
         </ul>
         {/if}
     </nav>
